4be2e5aab03566a3f2298b2f63c199bd50f4bf34
MihirCitrusbug <mihir.citrusbug@gmail.com>
2023-06-28 15:36:53 +0530

    JavaScript Test 2 completed

---
+++ js/task2.js
@@ -0,0 +1,130 @@
+$(document).ready(function () {
+    console.log("ready!");
+    let html = `<h2>First name: ` + localStorage.getItem("first_name") + `</h2>
+                <h2>Last name: `+ localStorage.getItem("last_name") + `</h2>
+                <h2>Email: `+ localStorage.getItem("email") + `</h2>
+                <h2>Gender: `+ localStorage.getItem("gender") + `</h2>
+                <h2>Age: `+ localStorage.getItem("age") + `</h2>`
+
+    $("#information_body").append(html)
+});
+
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
+
+function showTab(n) {
+    // This function will display the specified tab of the form ...
+    var x = document.getElementsByClassName("tab");
+    x[n].style.display = "block";
+    // ... and fix the Previous/Next buttons:
+    if (n == 0) {
+        document.getElementById("prevBtn").style.display = "none";
+    } else {
+        document.getElementById("prevBtn").style.display = "inline";
+    }
+    if (n == (x.length - 1)) {
+        $("#nextBtn").hide()
+        $("#submit_btn").show()
+    } else {
+        $("#nextBtn").show()
+        $("#submit_btn").hide()
+    }
+    // ... and run a function that displays the correct step indicator:
+    fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+    // This function will figure out which tab to display
+    var x = document.getElementsByClassName("tab");
+    // Exit the function if any field in the current tab is invalid:
+    if (n == 1 && !validateForm()) return false;
+    // Hide the current tab:
+    x[currentTab].style.display = "none";
+    // Increase or decrease the current tab by 1:
+    currentTab = currentTab + n;
+    // if you have reached the end of the form... :
+    if (currentTab >= x.length) {
+        //...the form gets submitted:
+        document.getElementById("regForm").submit();
+        return false;
+    }
+    // Otherwise, display the correct tab:
+    showTab(currentTab);
+    $("#submit_btn").prop('disabled', true)
+    $("#nextBtn").prop('disabled', true)
+}
+
+function validateForm() {
+    // This function deals with validation of the form fields
+    var x, y, i, valid = true;
+    x = document.getElementsByClassName("tab");
+    y = x[currentTab].getElementsByTagName("input");
+    // A loop that checks every input field in the current tab:
+    for (i = 0; i < y.length; i++) {
+        // If a field is empty...
+        if (y[i].value == "") {
+            // add an "invalid" class to the field:
+            y[i].className += " invalid";
+            // and set the current valid status to false:
+            valid = false;
+        }
+    }
+    // If the valid status is true, mark the step as finished and valid:
+    if (valid) {
+        document.getElementsByClassName("step")[currentTab].className += " finish";
+    }
+    return valid; // return the valid status
+}
+
+function fixStepIndicator(n) {
+    // This function removes the "active" class of all steps...
+    var i, x = document.getElementsByClassName("step");
+    for (i = 0; i < x.length; i++) {
+        x[i].className = x[i].className.replace(" active", "");
+    }
+    //... and adds the "active" class to the current step:
+    x[n].className += " active";
+}
+
+function name_validation() {
+    let first_name = $("#first_name").val()
+    let last_name = $("#last_name").val()
+    if (first_name == "" || first_name == null || last_name == "" || last_name == null) {
+        $("#nextBtn").prop('disabled', true)
+    }
+    else {
+        $("#nextBtn").prop('disabled', false)
+        localStorage.setItem("first_name", first_name)
+        localStorage.setItem("last_name", last_name)
+    }
+}
+
+function email_validation() {
+    let email_regex = /^([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|"([]!#-[^-~ \t]|(\\[\t -~]))+")@([0-9A-Za-z]([0-9A-Za-z-]{0,61}[0-9A-Za-z])?(\.[0-9A-Za-z]([0-9A-Za-z-]{0,61}[0-9A-Za-z])?)*|\[((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|IPv6:((((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){6}|::((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){5}|[0-9A-Fa-f]{0,4}::((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){4}|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):)?(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){3}|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,2}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){2}|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,3}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,4}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::)((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3})|(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3})|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,5}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3})|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,6}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::)|(?!IPv6:)[0-9A-Za-z-]*[0-9A-Za-z]:[!-Z^-~]+)])$/
+    let email = $("#email").val()
+    if (email == "" || email == null) {
+        $("#nextBtn").prop('disabled', true)
+    }
+    else if (email.match(email_regex)) {
+        $("#nextBtn").prop('disabled', false)
+        localStorage.setItem("email", email)
+    }
+    else {
+        $("#nextBtn").prop('disabled', true)
+    }
+}
+
+function myFunction(gender) {
+    localStorage.setItem("gender", gender)
+    $("#nextBtn").prop('disabled', false)
+}
+$("#age").on("input", function () {
+    let age = $(this).val()
+    if (age == "" || age == null) {
+        $("#submit_btn").prop('disabled', true)
+    }
+    else {
+        $("#submit_btn").prop('disabled', false)
+        localStorage.setItem("age", age)
+    }
+})
