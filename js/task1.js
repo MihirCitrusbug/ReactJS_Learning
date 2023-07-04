63177fca0d274d08855544c2b0588bd757955483
MihirCitrusbug <mihir.citrusbug@gmail.com>
2023-07-04 16:41:44 +0530

    changes in JS task

--- js/task1.js
+++ js/task1.js
@@ -1,139 +1,119 @@
 
-$(document).ready(function () {
-    console.log("ready!");
-});
-
-// First name validation
+// * First name validation
 function first_name_validation() {
-    let first_name = $("#first_name").val()
+    let first_name = document.getElementById("first_name").value.trim();
     if (first_name == "" || first_name == null) {
-        $("#first_name_error").show()
+        document.getElementById("first_name_error").style.display = "block";
         return false
     }
     else {
-        $("#first_name_error").hide()
+        document.getElementById("first_name_error").style.display = "none";
         return true
     }
 }
-$("#first_name").on("input", function () {
-    first_name_validation()
-});
 
-// Last name validation
+// * Last name validation
 function last_name_validation() {
-    let last_name = $("#last_name").val()
+    let last_name = document.getElementById('last_name').value.trim();
     if (last_name == "" || last_name == null) {
-        $("#last_name_error").show()
+        document.getElementById("last_name_error").style.display = "block";
         return false
     }
     else {
-        $("#last_name_error").hide()
+        document.getElementById("last_name_error").style.display = "none";
         return true
     }
 }
-$("#last_name").on("input", function () {
-    last_name_validation()
-});
 
-// Email validation
+// * Email validation
 function email_validation() {
     let email_regex = /^([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|"([]!#-[^-~ \t]|(\\[\t -~]))+")@([0-9A-Za-z]([0-9A-Za-z-]{0,61}[0-9A-Za-z])?(\.[0-9A-Za-z]([0-9A-Za-z-]{0,61}[0-9A-Za-z])?)*|\[((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|IPv6:((((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){6}|::((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){5}|[0-9A-Fa-f]{0,4}::((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){4}|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):)?(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){3}|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,2}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){2}|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,3}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,4}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::)((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3})|(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3})|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,5}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3})|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,6}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::)|(?!IPv6:)[0-9A-Za-z-]*[0-9A-Za-z]:[!-Z^-~]+)])$/
-    let email = $("#email").val()
+    let email = document.getElementById('email').value.trim();
+    let email_error = document.getElementById("email_error")
     if (email == "" || email == null) {
-        $("#email_error").text("this field is required")
-        $("#email_error").show()
+        email_error.textContent = "this field is required";
+        email_error.style.display = "block"
         return false
     }
     else {
-        $("#email_error").text("")
-        $("#email_error").hide()
+        email_error.textContent = "";
+        email_error.style.display = "none"
         if (email.match(email_regex)) {
-            $("#email_error").text("")
-            $("#email_error").hide()
+            email_error.textContent = "";
+            email_error.style.display = "none"
             return true
         } else {
-            $("#email_error").text("Invalid email address!")
-            $("#email_error").show()
+            email_error.textContent = "Invalid email address!";
+            email_error.style.display = "block"
             return false
         }
     }
 }
-$("#email").on("input", function () {
-    email_validation()
-});
 
-// Phone No. validation
+// * Phone No. validation
 function phoneNo_validation() {
     let phoneNo_regex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-    let phoneNo = $("#phoneNo").val();
+    let phoneNo = document.getElementById("phoneNo").value.trim();
+    let phoneNo_error = document.getElementById("phoneNo_error");
     if (phoneNo == "" || phoneNo == null) {
-        $("#phoneNo_error").text("this field is required")
-        $("#phoneNo_error").show()
+        phoneNo_error.textContent = "this field is required";
+        phoneNo_error.style.display = "block"
         return false
     }
     else {
-        $("#phoneNo_error").text("")
-        $("#phoneNo_error").hide()
+        phoneNo_error.textContent = "";
+        phoneNo_error.style.display = "none"
         if (phoneNo.match(phoneNo_regex)) {
-            $("#phoneNo_error").text("")
-            $("#phoneNo_error").hide()
+            phoneNo_error.textContent = "";
+            phoneNo_error.style.display = "none"
             return true
         } else {
-            $("#phoneNo_error").text("Invalid Phone number!")
-            $("#phoneNo_error").show()
+            phoneNo_error.textContent = "Invalid Phone number!";
+            phoneNo_error.style.display = "block"
             return false
         }
     }
 }
-$("#phoneNo").on("input", function () {
-    phoneNo_validation()
-})
 
-// Zip code validation
+// * Zip code validation
 function zipcode_validation() {
     let zipcode_regex = /(^\d{6}$)|(^\d{6}-\d{4}$)/;
-    let zipcode = $("#zipcode").val();
+    let zipcode = document.getElementById("zipcode").value.trim();
+    let zipcode_error = document.getElementById("zipcode_error")
     if (zipcode == "" || zipcode == null) {
-        $("#zipcode_error").text("this field is required")
-        $("#zipcode_error").show()
+        zipcode_error.textContent = "this field is required";
+        zipcode_error.style.display = "block"
         return false
     }
     else {
-        $("#zipcode_error").text("")
-        $("#zipcode_error").hide()
+        zipcode_error.textContent = "";
+        zipcode_error.style.display = "none"
         if (zipcode.match(zipcode_regex)) {
-            $("#zipcode_error").text("")
-            $("#zipcode_error").hide()
+            zipcode_error.textContent = "";
+            zipcode_error.style.display = "none"
             return true
         } else {
-            $("#zipcode_error").text("Invalid Zipcode!")
-            $("#zipcode_error").show()
+            zipcode_error.textContent = "Invalid Zipcode!"
+            zipcode_error.style.display = "block"
             return false
         }
     }
 }
-$("#zipcode").on("input", function () {
-    zipcode_validation()
-})
 
-// Date of Birth validation
+// * Date of Birth validation
 function dob_validation() {
-    let dob = $("#dob").val()
+    let dob = document.getElementById('dob').value
     if (dob == "" || dob == null) {
-        $("#dob_error").show()
+        document.getElementById("dob_error").style.display = "block"
         return false
     }
     else {
-        $("#dob_error").hide()
+        document.getElementById("dob_error").style.display = "none"
         return true
     }
 }
-$("#dob").on("input", function () {
-    dob_validation()
-});
 
-$("#submit_btn").on("click", function () {
-    console.log("submit button clicked")
+function submit_fuc() {
     let first_name_flag = first_name_validation()
     let last_name_flag = last_name_validation()
     let email_flag = email_validation()
@@ -147,5 +127,9 @@ $("#submit_btn").on("click", function () {
             'You clicked the button!',
             'success'
         )
+        document.querySelector(".swal2-confirm").addEventListener("click", (event) => {
+            window.location.reload();
+        });
     }
-})
+}
+
--- js/task2.js
+++ js/task2.js
@@ -1,129 +1,115 @@
-$(document).ready(function () {
-    let html = `<h2>First name: ` + localStorage.getItem("first_name") + `</h2>
-                <h2>Last name: `+ localStorage.getItem("last_name") + `</h2>
-                <h2>Email: `+ localStorage.getItem("email") + `</h2>
-                <h2>Gender: `+ localStorage.getItem("gender") + `</h2>
-                <h2>Age: `+ localStorage.getItem("age") + `</h2>`
 
-    $("#information_body").append(html)
-});
-
-var currentTab = 0; // Current tab is set to be the first tab (0)
+let currentTab = 0; // Current tab is set to be the first tab (0)
 showTab(currentTab); // Display the current tab
 
-function showTab(n) {
+function showTab(index) {
     // This function will display the specified tab of the form ...
-    var x = document.getElementsByClassName("tab");
-    x[n].style.display = "block";
+    let tabs = document.getElementsByClassName("tab");
+    tabs[index].style.display = "block";
     // ... and fix the Previous/Next buttons:
-    if (n == 0) {
+    if (index == (tabs.length - 1)) {
+        document.getElementById("nextBtn").style.display = "none";
+        document.getElementById("submit_btn").style.display = "inline";
+    } else if (index == 0) {
         document.getElementById("prevBtn").style.display = "none";
-    } else {
-        document.getElementById("prevBtn").style.display = "inline";
     }
-    if (n == (x.length - 1)) {
-        $("#nextBtn").hide()
-        $("#submit_btn").show()
-    } else {
-        $("#nextBtn").show()
-        $("#submit_btn").hide()
+    else {
+        document.getElementById("prevBtn").style.display = "inline";
+        document.getElementById("nextBtn").style.display = "inline";
+        document.getElementById("submit_btn").style.display = "none";
     }
     // ... and run a function that displays the correct step indicator:
-    fixStepIndicator(n)
+    fixStepIndicator(index)
 }
 
-function nextPrev(n) {
-    // This function will figure out which tab to display
-    var x = document.getElementsByClassName("tab");
-    // Exit the function if any field in the current tab is invalid:
-    if (n == 1 && !validateForm()) return false;
-    // Hide the current tab:
-    x[currentTab].style.display = "none";
-    // Increase or decrease the current tab by 1:
-    currentTab = currentTab + n;
-    // if you have reached the end of the form... :
-    if (currentTab >= x.length) {
-        //...the form gets submitted:
-        document.getElementById("regForm").submit();
+function nextPrev(index) {
+    let tabs = document.getElementsByClassName("tab");
+    if (index == 1 && !validateForm()) {
         return false;
     }
-    // Otherwise, display the correct tab:
+
+    // Hide the current tab:
+    tabs[currentTab].style.display = "none";
+    currentTab += index;
     showTab(currentTab);
-    $("#submit_btn").prop('disabled', true)
-    $("#nextBtn").prop('disabled', true)
 }
 
 function validateForm() {
     // This function deals with validation of the form fields
-    var x, y, i, valid = true;
-    x = document.getElementsByClassName("tab");
-    y = x[currentTab].getElementsByTagName("input");
+    var tabs, inputs, i, isValid = true;
+    tabs = document.getElementsByClassName("tab");
+    inputs = tabs[currentTab].getElementsByTagName("input");
     // A loop that checks every input field in the current tab:
-    for (i = 0; i < y.length; i++) {
+    for (i = 0; i < inputs.length; i++) {
         // If a field is empty...
-        if (y[i].value == "") {
-            // add an "invalid" class to the field:
-            y[i].className += " invalid";
-            // and set the current valid status to false:
-            valid = false;
+        if (inputs[i].value.trim() == "" || inputs[i].value.trim() == undefined) {
+            inputs[i].classList.add("invalid");
+            isValid = false;
+        }
+        else {
+            inputs[i].classList.remove("invalid")
+        }
+        if (inputs[i].getAttribute('type') == "email") {
+            if (!email_validation()) {
+                inputs[i].classList.add("invalid")
+                isValid = false;
+            }
+        }
+        else if (inputs[i].getAttribute('type') == "radio" && !document.querySelector('input[name="gender"]:checked')) {
+            document.getElementById("gender_error").style.display = "block"
+            isValid = false;
         }
     }
-    // If the valid status is true, mark the step as finished and valid:
-    if (valid) {
-        document.getElementsByClassName("step")[currentTab].className += " finish";
+    // If the isValid status is true, mark the step as finished and isValid:
+    if (isValid) {
+        document.getElementsByClassName("step")[currentTab].classList.add("finish");
     }
-    return valid; // return the valid status
+    return isValid; // return the isValid status
 }
 
-function fixStepIndicator(n) {
-    // This function removes the "active" class of all steps...
-    var i, x = document.getElementsByClassName("step");
-    for (i = 0; i < x.length; i++) {
-        x[i].className = x[i].className.replace(" active", "");
-    }
-    //... and adds the "active" class to the current step:
-    x[n].className += " active";
+function fixStepIndicator(index) {
+    const steps = document.querySelectorAll(".step");
+    steps.forEach(step => step.classList.remove("active"));
+    steps[index].classList.add("active");
 }
 
 function name_validation() {
-    let first_name = $("#first_name").val()
-    let last_name = $("#last_name").val()
-    if (first_name == "" || first_name == null || last_name == "" || last_name == null) {
-        $("#nextBtn").prop('disabled', true)
-    }
-    else {
-        $("#nextBtn").prop('disabled', false)
-        localStorage.setItem("first_name", first_name)
-        localStorage.setItem("last_name", last_name)
+    const firstName = document.getElementById("first_name")?.value?.trim();
+    const lastName = document.getElementById("last_name")?.value?.trim();
+
+    if (firstName && lastName) {
+        localStorage.setItem("first_name", firstName);
+        localStorage.setItem("last_name", lastName);
     }
 }
 
 function email_validation() {
     let email_regex = /^([-!#-'*+/-9=?A-Z^-~]+(\.[-!#-'*+/-9=?A-Z^-~]+)*|"([]!#-[^-~ \t]|(\\[\t -~]))+")@([0-9A-Za-z]([0-9A-Za-z-]{0,61}[0-9A-Za-z])?(\.[0-9A-Za-z]([0-9A-Za-z-]{0,61}[0-9A-Za-z])?)*|\[((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|IPv6:((((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){6}|::((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){5}|[0-9A-Fa-f]{0,4}::((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){4}|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):)?(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){3}|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,2}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){2}|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,3}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,4}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::)((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3})|(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3})|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,5}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3})|(((0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}):){0,6}(0|[1-9A-Fa-f][0-9A-Fa-f]{0,3}))?::)|(?!IPv6:)[0-9A-Za-z-]*[0-9A-Za-z]:[!-Z^-~]+)])$/
-    let email = $("#email").val()
+    let email = document.getElementById('email').value.trim();
     if (email == "" || email == null) {
-        $("#nextBtn").prop('disabled', true)
+        return false
     }
     else if (email.match(email_regex)) {
-        $("#nextBtn").prop('disabled', false)
         localStorage.setItem("email", email)
+        return true
     }
     else {
-        $("#nextBtn").prop('disabled', true)
+        return false
     }
 }
 
 function myFunction(gender) {
-    localStorage.setItem("gender", gender)
-    $("#nextBtn").prop('disabled', false)
+    document.getElementById("gender_error").style.display = "none";
+    localStorage.setItem("gender", gender);
 }
-$("#age").on("input", function () {
-    let age = $(this).val()
+
+function age_validation() {
+    let age = document.getElementById("age").value.trim();
     if (age == "" || age == null) {
-        $("#submit_btn").prop('disabled', true)
+        document.getElementById("submit_btn").disabled = true;
     }
     else {
-        $("#submit_btn").prop('disabled', false)
+        document.getElementById("submit_btn").disabled = false;
         localStorage.setItem("age", age)
     }
-})
+}
